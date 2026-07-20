829e44fdc167b6cfbcf7966c6725ebe2ab5d2ba0
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2026-07-20 08:39:24 +0530

    JS - strong implementation of polyfill of bind

--- 04_JavaScript/Practice Questions/this.js
+++ 04_JavaScript/Practice Questions/this.js
@@ -32,5 +32,25 @@ Function.prototype.myApply = function (ctx, argsArr = []) {
   }
 };
 
+Function.prototype.myBind = function (ctx, ...args1) {
+  if (typeof this !== "function") {
+    throw new TypeError("myBind must be called on a function");
+  }
+  ctx = ctx == null ? globalThis : Object(ctx);
+  const fn = this;
+
+  function boundFn(...args2) {
+    if (new.target) {
+      return fn.myCall(this, ...args1, ...args2);
+    } else {
+      return fn.myCall(ctx, ...args1, ...args2);
+    }
+  }
+
+  boundFn.prototype = Object.create(fn.prototype);
+
+  return boundFn;
+};
+
 // Review
 // https://chatgpt.com/g/g-p-6a49c38571148191bf064b777151e370-software-engineering-upskilling-2026/c/6a5c7c6f-fd30-83e8-9fa5-2226ca132d35
